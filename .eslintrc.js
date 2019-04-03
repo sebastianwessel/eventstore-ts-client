382e31e53e1346e86754c439b2e0120f68bd73d7
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-03 21:20:28 +0200

    Update project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins:[
+    "json",
+    "prettier"
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
@@ -10,7 +14,12 @@ module.exports = {
       sourceType: 'module', // Allows for the use of imports
   },
   rules: {
-      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-      // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "semi": "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        "parser": "typescript"
+      }
+    ]
   },
 };
