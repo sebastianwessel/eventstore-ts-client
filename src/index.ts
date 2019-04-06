ca6e4c1d3d791254306c2bb429719987afbfedf3
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-06 16:31:49 +0200

    Refine module export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export {Eventstore, EventstoreSettings} from './eventstore'
-export {Stream} from './stream'
 export {Event} from './event'
+export {ExpectedVersion} from './protobuf/ExpectedVersion'
