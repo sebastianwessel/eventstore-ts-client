cf8dd6337fb8f7af668c8e8ff091fa38a2a2133a
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-08 22:38:20 +0200

    Fix nyc pipeline settings

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -3,7 +3,4 @@ echo "Combine test results"
 cp -a ./.nyc_integration/. ./.nyc_output/
 cp -a ./.nyc_unit/. ./.nyc_output/
 nyc merge .nyc_output ./coverage/coverage.json
-ls -a ./.nyc_output
-echo  "***********"
-ls -a ./coverage/
-nyc report --reporter=lcov --reporter=text --report-dir=./coverage --temp-directory=./.nyc_output
+nyc report
