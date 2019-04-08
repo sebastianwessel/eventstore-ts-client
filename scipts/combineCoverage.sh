dadef589f19cc1dbb75eccb585ef0f9dd802c20a
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-08 22:27:53 +0200

    Debug pipline coverage

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 echo "Combine test results"
-ls -a
 cp -a ./.nyc_integration/. .nyc_output/
 cp -a ./.nyc_unit/. .nyc_output/
 nyc merge .nyc_output ./coverage/coverage.json
-nyc report --reporter=lcov --reporter=text
+ls -a ./.nyc_output
+echo  "***********"
+ls -a ./coverage/
+nyc report --reporter=lcov --reporter=text --report-dir=./coverage --temp-directory=./.nyc_output
