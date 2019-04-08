5b1031c2f3681fa44cebc9d563c84739cb5dfb36
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-08 22:28:26 +0200

    Fix

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Combine test results"
-cp -a ./.nyc_integration/. .nyc_output/
-cp -a ./.nyc_unit/. .nyc_output/
+cp -a ./.nyc_integration/. ./.nyc_output/
+cp -a ./.nyc_unit/. ./.nyc_output/
 nyc merge .nyc_output ./coverage/coverage.json
 ls -a ./.nyc_output
 echo  "***********"
