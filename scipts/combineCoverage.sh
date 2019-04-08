8341936b92b24858397dd4f148e07c1e0f7c4195
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-08 21:47:31 +0200

    Fix combine coverage script

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 echo "Combine test results"
-mkdir .nyc_output
 cp .nyc_integration/*.* .nyc_output
 cp .nyc_unit/*.* .nyc_output
-mkdir coverage
 nyc merge .nyc_output ./coverage/coverage.json
 nyc report --reporter=text --reporter=lcov
-rm -r .nyc_integration
-rm -r .nyc_unit
