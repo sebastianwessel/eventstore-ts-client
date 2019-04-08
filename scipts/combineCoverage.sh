ae4f00a6fb7dcffdd0395e5789cb7d9000d0279e
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-08 22:16:52 +0200

    Debug script for travis-ci

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 echo "Combine test results"
-cp .nyc_integration/*.* .nyc_output
-cp .nyc_unit/*.* .nyc_output
+ls -a
+cp -a ./.nyc_integration/. .nyc_output/
+cp -a ./.nyc_unit/. .nyc_output/
 nyc merge .nyc_output ./coverage/coverage.json
 nyc report --reporter=lcov --reporter=text
