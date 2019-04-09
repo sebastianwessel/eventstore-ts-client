0977bd01521c789059ab37acf49c6b3f2067797a
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-09 16:11:21 +0200

    Fix combine script for pipeline

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "Combine test results"
-mkdir ./.nyc_output
+mkdir -p ./.nyc_output
 nyc merge ./.nyc_integration ./.nyc_output/integation.json
-rm -r ./.nyc_integration
+#rm -r ./.nyc_integration
 nyc merge ./.nyc_unit ./.nyc_output/unit.json
-rm -r ./.nyc_unit
+#rm -r ./.nyc_unit
 
-rm ./docs/coverage.json > /dev/null
+#rm ./docs/coverage.json > /dev/null
 
