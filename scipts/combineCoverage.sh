7ac9004ea2387ddbe7e838b86b89d3721e4d0f32
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-09 15:49:07 +0200

    Refine coverage report setup

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 echo "Combine test results"
+mkdir ./.nyc_output
 nyc merge ./.nyc_integration ./.nyc_output/integation.json
 nyc merge ./.nyc_unit ./.nyc_output/unit.json
 #nyc merge ./.nyc_output ./.nyc_output/combined.json
 
 rm ./docs/coverage.json
-#rm ./.nyc_output/integation.json
-#rm ./.nyc_output/unit.json
+rm -r ./.nyc_integration
+rm -r ./.nyc_output
+rm -r ./.nyc_unit
