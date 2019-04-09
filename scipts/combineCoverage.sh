41ff960742f76e3b6aab980ba04a23100c0724fc
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-09 15:32:04 +0200

    Downgrade nyc version

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -2,7 +2,7 @@
 echo "Combine test results"
 nyc merge ./.nyc_integration ./.nyc_output/integation.json
 nyc merge ./.nyc_unit ./.nyc_output/unit.json
-nyc merge ./.nyc_output ./.nyc_output/combined.json
+#nyc merge ./.nyc_output ./.nyc_output/combined.json
 
 rm ./docs/coverage.json
 #rm ./.nyc_output/integation.json
