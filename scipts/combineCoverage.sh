2fc4a0eac8f5da002f394474ef1d1056f2464907
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-09 09:00:31 +0200

    Run tests at once in pipeline

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 echo "Combine test results"
-cp -a ./.nyc_integration/. ./.nyc_output/
-cp -a ./.nyc_unit/. ./.nyc_output/
+nyc merge ./.nyc_integration ./.nyc_output/integation.json
+#nyc merge ./.nyc_unit ./.nyc_output/unit.json
+#nyc merge ./.nyc_output ./.nyc_output/combined.json
+#rm ./.nyc_output/integation.json
+#rm ./.nyc_output/unit.json
