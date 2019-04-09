f379d75f6de509f346fc41e2070300c92d27914e
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-09 16:01:28 +0200

    Update pipeline

--- scipts/combineCoverage.sh
+++ scipts/combineCoverage.sh
@@ -2,10 +2,9 @@
 echo "Combine test results"
 mkdir ./.nyc_output
 nyc merge ./.nyc_integration ./.nyc_output/integation.json
-nyc merge ./.nyc_unit ./.nyc_output/unit.json
-#nyc merge ./.nyc_output ./.nyc_output/combined.json
-
-rm ./docs/coverage.json
 rm -r ./.nyc_integration
-rm -r ./.nyc_output
+nyc merge ./.nyc_unit ./.nyc_output/unit.json
 rm -r ./.nyc_unit
+
+rm ./docs/coverage.json > /dev/null
+
