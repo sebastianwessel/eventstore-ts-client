80c50cd39fc20f854675c3507e0cf4147d54942a
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-16 21:55:25 +0200

    Order tests

--- test/integrationTests/2_stream/2_streamMetadata.test.ts
+++ test/integrationTests/2_stream/2_streamMetadata.test.ts
@@ -1,6 +1,7 @@
-import * as assert from 'assert'
 import {Eventstore} from '../../../src'
-describe('Read stream basic tests', (): void => {
+import * as assert from 'assert'
+
+describe('Stream tests', (): void => {
   const es = new Eventstore({
     uri: 'discover://restrictedUser:restrictedOnlyUserPassword@cluster1.escluster.net:2112',
     clientId: 'ts-client-test',
@@ -17,53 +18,6 @@ describe('Read stream basic tests', (): void => {
       await es.disconnect()
     }
   )
-
-  describe('Read one event', async (): Promise<void> => {
-    it('returns a event by given eventNumber', async (): Promise<void> => {
-      const event = await es
-        .stream('teneventsstream-ad44caa8-d701-48f2-ac1e-2ec147ff1df5')
-        .getEventByNumber(0, true)
-      const data: {count?: number} = event.data
-
-      assert.strictEqual(event.name, 'EventTypeOne')
-      assert.notEqual(event.data, null)
-      assert.strictEqual(data.count, 1)
-    })
-
-    it('returns a event by given eventNumber from master', async (): Promise<void> => {
-      const event = await es
-        .stream('teneventsstream-ad44caa8-d701-48f2-ac1e-2ec147ff1df5')
-        .requiresMaster()
-        .getEventByNumber(0, true)
-      const data: {count?: number} = event.data
-
-      assert.strictEqual(event.name, 'EventTypeOne')
-      assert.notEqual(event.data, null)
-      assert.strictEqual(data.count, 1)
-    })
-
-    it('returns first event of stream', async (): Promise<void> => {
-      const event = await es
-        .stream('teneventsstream-ad44caa8-d701-48f2-ac1e-2ec147ff1df5')
-        .getFirstEvent()
-      const data: {count?: number} = event.data
-
-      assert.strictEqual(event.name, 'EventTypeOne')
-      assert.notEqual(event.data, null)
-      assert.strictEqual(data.count, 1)
-    })
-
-    it('returns last event of stream', async (): Promise<void> => {
-      const event = await es
-        .stream('teneventsstream-ad44caa8-d701-48f2-ac1e-2ec147ff1df5')
-        .getLastEvent()
-      const data: {count?: number} = event.data
-
-      assert.strictEqual(event.name, 'EventTypeTwo')
-      assert.notEqual(event.data, null)
-      assert.strictEqual(data.count, 10)
-    })
-  })
 
   describe('Get stream metadata', async (): Promise<void> => {
     it('returns null for existing stream with no metadata', async (): Promise<void> => {
@@ -95,7 +49,6 @@ describe('Read stream basic tests', (): void => {
       }
     })
   })
-
   describe('Set stream metadata', async (): Promise<void> => {
     it('writes stream metadata', async (): Promise<void> => {
       const testData = {foo: 'bar'}
