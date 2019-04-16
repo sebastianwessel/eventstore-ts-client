f3af66021373e51607cc90a353ed2bbac3564994
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-16 22:17:23 +0200

    Fix test compare result

--- test/unitTests/1_eventstore/position.test.ts
+++ test/unitTests/1_eventstore/position.test.ts
@@ -5,13 +5,13 @@ import Long = require('long')
 describe('Test position class', (): void => {
   it('creates a new instance from numbers', (): void => {
     const newPosition = new Position(0, 0)
-    assert.strictEqual(newPosition.commitPosition.compare(0), true)
-    assert.strictEqual(newPosition.preparePosition.compare(0), true)
+    assert.strictEqual(newPosition.commitPosition.compare(0), 0)
+    assert.strictEqual(newPosition.preparePosition.compare(0), 0)
   })
 
   it('creates a new instance from Long', (): void => {
     const newPosition = new Position(Long.fromValue(0), Long.fromValue(0))
-    assert.strictEqual(newPosition.commitPosition.compare(0), true)
-    assert.strictEqual(newPosition.preparePosition.compare(0), true)
+    assert.strictEqual(newPosition.commitPosition.compare(0), 0)
+    assert.strictEqual(newPosition.preparePosition.compare(0), 0)
   })
 })
