7d04bd99acdcfb5eb87b928df1b1620875b43dc4
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-18 14:18:46 +0200

    Add helper script to create testdata

---
+++ scripts/generateTestdata.js
@@ -0,0 +1,47 @@
+const uuid = require('uuid4')
+const fs = require('fs')
+const path = require('path')
+
+const outputDir = path.resolve(__dirname, '../test/integrationTests/testSetup/testStreams')
+
+const eventNameList = [
+  'EventA',
+  'EventB',
+  'EventC',
+  'EventD',
+  'EventE',
+  'EventF',
+  'EventG',
+  'EventH',
+  'EventI',
+  'EventJ'
+]
+
+const generateHundredFile = (fileName) => {
+  const testData = []
+  let count = 0
+  for (let x = 0, xMax = 100; x < xMax; x++) {
+    eventNameList.forEach((name) => {
+      testData.push({
+        eventId: uuid(),
+        eventType: name,
+        data: {
+          count
+        }
+      })
+      count++
+    })
+  }
+
+  fs.writeFileSync(path.resolve(outputDir, fileName), JSON.stringify(testData))
+}
+
+const generateTenThousand = () => {
+  const streamName = 'tenthousandstream-' + uuid()
+  fs.mkdirSync(path.resolve(outputDir, streamName))
+  for (let x = 0, xMax = 10; x < xMax; x++) {
+    generateHundredFile(streamName + '/' + x + '.json')
+  }
+}
+
+generateTenThousand()
