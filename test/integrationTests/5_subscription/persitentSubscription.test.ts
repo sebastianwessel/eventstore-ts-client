2203293042c24723535a671141b1c3d3cdea78d7
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-24 15:28:20 +0200

    Update tests

--- test/integrationTests/5_subscription/persitentSubscription.test.ts
+++ test/integrationTests/5_subscription/persitentSubscription.test.ts
@@ -20,7 +20,7 @@ describe('Persitent subscription test', (): void => {
     )
 
     it('creates a persistent subscription', async (): Promise<void> => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         await stream
           .withCredentials({username: 'admin', password: 'changeit'})
@@ -31,7 +31,7 @@ describe('Persitent subscription test', (): void => {
     })
 
     it('updates a persistent subscription', async (): Promise<void> => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         const subscription = await stream
           .withCredentials({username: 'admin', password: 'changeit'})
@@ -45,7 +45,7 @@ describe('Persitent subscription test', (): void => {
     it('throws on creating persistent subscription with same names again', async (): Promise<
       void
     > => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         await stream.createPersistentSubscription('persitentsubscription')
         assert.fail('has not thrown')
@@ -55,7 +55,7 @@ describe('Persitent subscription test', (): void => {
     })
 
     it('deletes a persistent subscription', async (): Promise<void> => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         const subscription = await stream
           .withCredentials({username: 'admin', password: 'changeit'})
@@ -75,7 +75,7 @@ describe('Persitent subscription test', (): void => {
     before(
       async (): Promise<void> => {
         await es.connect()
-        const stream = await es
+        const stream = es
           .stream('subscribestream')
           .withCredentials({username: 'admin', password: 'changeit'})
         await stream.createPersistentSubscription('persitentsubscription2')
@@ -89,7 +89,7 @@ describe('Persitent subscription test', (): void => {
     )
 
     it('throws on create', async (): Promise<void> => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         await stream.createPersistentSubscription('persitentsubscription2')
         assert.fail('has not thrown')
@@ -99,7 +99,7 @@ describe('Persitent subscription test', (): void => {
     })
 
     it('throws on update', async (): Promise<void> => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         const subscription = await stream
           .withCredentials({username: 'admin', password: 'changeit'})
@@ -111,7 +111,7 @@ describe('Persitent subscription test', (): void => {
     })
 
     it('throws on delete', async (): Promise<void> => {
-      const stream = await es.stream('subscribestream')
+      const stream = es.stream('subscribestream')
       try {
         const subscription = await stream
           .withCredentials({username: 'admin', password: 'changeit'})
@@ -120,6 +120,55 @@ describe('Persitent subscription test', (): void => {
       } catch (err) {
         assert.strictEqual(err.name, 'EventstoreAccessDeniedError')
       }
+    })
+  })
+
+  describe('Persistent subscription get events', (): void => {
+    const es = new Eventstore({
+      uri: 'discover://restrictedUser:restrictedOnlyUserPassword@cluster1.escluster.net:2112',
+      clientId: 'ts-client-test'
+    })
+    before(
+      async (): Promise<void> => {
+        await es.connect()
+        await es
+          .stream('persitentsubscribestream')
+          .createPersistentSubscription(
+            'persitentsubscription',
+            {},
+            {username: 'admin', password: 'changeit'}
+          )
+      }
+    )
+
+    after(
+      async (): Promise<void> => {
+        await es.disconnect()
+      }
+    )
+
+    it('can start a subscription on empty stream', async (): Promise<void> => {
+      const subscription = es
+        .stream('persitentsubscribestream')
+        .getPersitentSubscription('persitentsubscription')
+      await subscription.start()
+      assert.strictEqual(
+        subscription.name,
+        `PersitentSubsbscription: persitentsubscribestream :: persitentsubscription`
+      )
+    })
+
+    it('can start a subscription on none empty stream', async (): Promise<void> => {
+      const newEvent = new Event('SomeEvent')
+      const stream = es.stream('persitentsubscribestream')
+      await stream.append(newEvent)
+      const subscription = stream.getPersitentSubscription('persitentsubscription')
+      await subscription.start()
+      await new Promise(
+        async (resolve): Promise<void> => {
+          setTimeout(resolve, 3000)
+        }
+      )
     })
   })
 })
