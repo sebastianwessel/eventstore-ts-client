311c46b939907291360baf7edbeb2af747420a5e
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-25 17:54:13 +0200

    Add NakAction consts

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,6 @@ export {
   PersistentSubscriptionConfig,
   SystemConsumerStrategies,
   SubscriptionStatus,
-  SubscriptionDropReason
+  SubscriptionDropReason,
+  NakAction
 } from './subscription'
