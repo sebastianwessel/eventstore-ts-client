0c73aaed8be091cb60fd8511ec996ecfa62e45db
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2019-04-30 11:29:41 +0200

    Export stream

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export {
 export {Event} from './event'
 export {ExpectedVersion} from './protobuf/ExpectedVersion'
 export {uuidToBuffer, uuidFromBuffer} from './protobuf/uuidBufferConvert'
-export {StreamPosition} from './stream'
+export {StreamPosition, Stream} from './stream'
 export {
   PersistentSubscriptionConfig,
   SystemConsumerStrategies,
